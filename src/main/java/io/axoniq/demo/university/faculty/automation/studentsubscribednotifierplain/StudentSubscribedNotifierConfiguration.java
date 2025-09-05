27ea949f821b836117ddadc9f3049985205faf6a
Mateusz Nowak <mateusz@nakodach.pl>
2025-09-05 16:25:06 +0200

    ✨ feat: add automation to notify students upon course subscription
    - Introduced `WhenStudentSubscribedThenSendNotification` to handle `StudentSubscribedToCourse` events.
    - Added `StudentSubscribedNotifierConfiguration` to configure the new automation.
    - Updated `FacultyModuleConfiguration` to include the notifier configuration.
    - Added tests for the notification automation.

---
+++ src/main/java/io/axoniq/demo/university/faculty/automation/studentsubscribednotifierplain/StudentSubscribedNotifierConfiguration.java
@@ -0,0 +1,33 @@
+package io.axoniq.demo.university.faculty.automation.studentsubscribednotifierplain;
+
+import io.axoniq.demo.university.faculty.events.StudentSubscribedToCourse;
+import org.axonframework.eventhandling.SimpleEventHandlingComponent;
+import org.axonframework.eventhandling.configuration.EventProcessorModule;
+import org.axonframework.eventhandling.processors.streaming.pooled.PooledStreamingEventProcessorModule;
+import org.axonframework.eventhandling.processors.streaming.token.GlobalSequenceTrackingToken;
+import org.axonframework.eventhandling.sequencing.SequentialPolicy;
+import org.axonframework.eventsourcing.configuration.EventSourcingConfigurer;
+import org.axonframework.messaging.QualifiedName;
+
+import java.util.concurrent.CompletableFuture;
+
+public class StudentSubscribedNotifierConfiguration {
+
+    public static EventSourcingConfigurer configure(EventSourcingConfigurer configurer) {
+        PooledStreamingEventProcessorModule automationProcessor = EventProcessorModule
+                .pooledStreaming("Automation_WhenStudentSubscribedThenSendNotification_Processor")
+                .eventHandlingComponents(
+                        c -> c.declarative(cfg -> SimpleEventHandlingComponent.builder()
+                                .sequencingPolicy(SequentialPolicy.INSTANCE)
+                                .handles(new QualifiedName(StudentSubscribedToCourse.class), WhenStudentSubscribedThenSendNotification::react).build())
+                )
+                // Due to a minor bug in the InMemoryEventStorageEngine this customization is needed if you want to use the implementation in the tests
+                .customized((c, cus) -> cus.initialToken(s -> CompletableFuture.completedFuture(new GlobalSequenceTrackingToken(0))));
+
+        return configurer
+                .modelling(modelling -> modelling.messaging(messaging -> messaging.eventProcessing(eventProcessing ->
+                        eventProcessing.pooledStreaming(ps -> ps.processor(automationProcessor))
+                )));
+    }
+
+}

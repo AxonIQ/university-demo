ab4b67ed0721f26178470143de7c890281ec3d48
Mateusz Nowak <mateusz@nakodach.pl>
2025-09-05 14:03:19 +0200

    ♻️ refactor: remove unused `MessageStream.Empty` and simplify notification event handler

--- src/main/java/io/axoniq/demo/university/faculty/automation/studentsubscribednotifier/WhenStudentSubscribedThenSendNotification.java
+++ src/main/java/io/axoniq/demo/university/faculty/automation/studentsubscribednotifier/WhenStudentSubscribedThenSendNotification.java
@@ -3,7 +3,6 @@ package io.axoniq.demo.university.faculty.automation.studentsubscribednotifier;
 import io.axoniq.demo.university.faculty.events.StudentSubscribedToCourse;
 import io.axoniq.demo.university.shared.application.notifier.NotificationService;
 import org.axonframework.eventhandling.annotation.EventHandler;
-import org.axonframework.messaging.MessageStream;
 
 /**
  * Automation that reacts on {@link StudentSubscribedToCourse} events and sends a notification.
@@ -18,13 +17,12 @@ public class WhenStudentSubscribedThenSendNotification {
     }
 
     @EventHandler
-    MessageStream.Empty<?> react(StudentSubscribedToCourse event) {
+    void react(StudentSubscribedToCourse event) {
         var notification = new NotificationService.Notification(
                 event.studentId().toString(),
                 "You have subscribed to course " + event.courseId()
         );
         notificationService.sendNotification(notification);
-        return MessageStream.empty();
     }
 
 }

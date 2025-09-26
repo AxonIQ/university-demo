bb1d994bbf5be5391fb0ad4e591d163862c90927
Mateusz Nowak <mateusz@nakodach.pl>
2025-09-26 18:47:41 +0200

    CourseStatsConfiguration - add query handling configuration

--- src/main/java/io/axoniq/demo/university/faculty/read/coursestats/CourseStatsConfiguration.java
+++ src/main/java/io/axoniq/demo/university/faculty/read/coursestats/CourseStatsConfiguration.java
@@ -3,6 +3,7 @@ package io.axoniq.demo.university.faculty.read.coursestats;
 import org.axonframework.eventhandling.configuration.EventProcessorModule;
 import org.axonframework.eventhandling.processors.streaming.pooled.PooledStreamingEventProcessorModule;
 import org.axonframework.eventsourcing.configuration.EventSourcingConfigurer;
+import org.axonframework.queryhandling.configuration.QueryHandlingModule;
 
 class CourseStatsConfiguration {
 
@@ -13,9 +14,14 @@ class CourseStatsConfiguration {
                         c -> c.annotated(cfg -> new CoursesStatsProjection(cfg.getComponent(CourseStatsRepository.class)))
                 ).notCustomized();
 
+        QueryHandlingModule getCourseStatsByIdQueryHandler = QueryHandlingModule.named("get-course-stats-by-id")
+                .queryHandlers()
+                .annotatedQueryHandlingComponent(cfg -> new GetCourseStatsByIdQueryHandler(cfg.getComponent(CourseStatsRepository.class)))
+                .build();
+
         return configurer
                 .componentRegistry(cr -> cr.registerComponent(CourseStatsRepository.class, cfg -> new InMemoryCourseStatsRepository()))
-                .componentRegistry(cr -> cr.registerComponent(GetCourseStatsByIdQueryHandler.class, cfg -> new GetCourseStatsByIdQueryHandler(cfg.getComponent(CourseStatsRepository.class))))
+                .registerQueryHandlingModule(getCourseStatsByIdQueryHandler)
                 .modelling(modelling -> modelling.messaging(messaging -> messaging.eventProcessing(eventProcessing ->
                         eventProcessing.pooledStreaming(ps -> ps.processor(projectionProcessor))
                 )));

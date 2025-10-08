e3868b18746bbefc89be5b4cb0e144c7cb5120ce
Steven van Beelen <steven.vanbeelen@axoniq.io>
2025-10-08 15:37:06 +0200

    Add empty base branch for those that want to start fresh
    Add empty base branch for those that want to start fresh

--- src/main/java/io/axoniq/demo/university/faculty/automation/allcoursesfullybookednotifier/SendAllCoursesFullyBookedNotification.java
+++
@@ -1,5 +0,0 @@
-package io.axoniq.demo.university.faculty.automation.allcoursesfullybookednotifier;
-
-public record SendAllCoursesFullyBookedNotification(String facultyId) {
-
-}
--- src/main/java/io/axoniq/demo/university/faculty/write/changecoursecapacity/ChangeCourseCapacity.java
+++
@@ -1,8 +0,0 @@
-package io.axoniq.demo.university.faculty.write.changecoursecapacity;
-
-import io.axoniq.demo.university.shared.ids.CourseId;
-import org.axonframework.modelling.annotations.TargetEntityId;
-
-public record ChangeCourseCapacity(@TargetEntityId CourseId courseId, int capacity) {
-
-}

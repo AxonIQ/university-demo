1019c4d9625bbc17af82c3646b01d8457cf50774
Simon Zambrovski <simon.zambrovski@holisticon.de>
2025-10-24 17:22:42 +0200

    change capacity

--- src/main/java/io/axoniq/demo/university/faculty/write/changecoursecapacity/ChangeCourseCapacityController.java
+++ src/main/java/io/axoniq/demo/university/faculty/write/changecoursecapacity/ChangeCourseCapacityController.java
@@ -1,5 +1,6 @@
-package io.axoniq.demo.university.faculty.write.renamecourse;
+package io.axoniq.demo.university.faculty.write.changecoursecapacity;
 
+import io.axoniq.demo.university.faculty.write.renamecourse.RenameCourse;
 import io.axoniq.demo.university.shared.ids.CourseId;
 import org.axonframework.commandhandling.gateway.CommandGateway;
 import org.springframework.http.HttpStatus;
@@ -15,13 +16,13 @@ import java.util.logging.Logger;
  */
 @RestController
 @RequestMapping("/api/courses")
-public class RenameCourseController {
+public class ChangeCourseCapacityController {
 
-  private static final Logger logger = Logger.getLogger(RenameCourseController.class.getName());
+  private static final Logger logger = Logger.getLogger(ChangeCourseCapacityController.class.getName());
 
   private final CommandGateway commandGateway;
 
-  public RenameCourseController(CommandGateway commandGateway) {
+  public ChangeCourseCapacityController(CommandGateway commandGateway) {
     this.commandGateway = commandGateway;
   }
 
@@ -36,20 +37,20 @@ public class RenameCourseController {
    * }
    * <p>
    * Example with curl:
-   * curl -X PUT http://localhost:8080/api/courses/my-course-123/rename \
+   * curl -X PUT http://localhost:8080/api/courses/my-course-123/capacity \
    * -H "Content-Type: application/json" \
    * -d '{"name":"Event Sourcing 101"}'
    */
-  @PutMapping("/{courseId}/rename")
-  public ResponseEntity<Void> renameCourse(@PathVariable("courseId") String courseId, @RequestBody RenameCourseRequest request) {
-    logger.info("Renaming course: " + request.name() + " to " + request.name);
+  @PutMapping("/{courseId}/capacity")
+  public ResponseEntity<Void> renameCourse(@PathVariable("courseId") String courseId, @RequestBody ChangeCourseCapacityRequest request) {
+    logger.info("Changing course capacity to: " + request.capacity());
 
     try {
-      RenameCourse command = new RenameCourse(new CourseId(courseId), request.name());
+      ChangeCourseCapacity command = new ChangeCourseCapacity(new CourseId(courseId), request.capacity);
 
       commandGateway.sendAndWait(command);
 
-      logger.info("Successfully renamed course with ID: " + courseId);
+      logger.info("Successfully changed capacity of course with ID: " + courseId);
 
       return ResponseEntity
         .status(HttpStatus.NO_CONTENT)
@@ -64,12 +65,12 @@ public class RenameCourseController {
   }
 
   /**
-   * Request DTO for renaming a course.
+   * Request DTO for changing capacity of a course.
    *
-   * @param name The name of the course (required)
+   * @param capacity The capacity of the course (required)
    */
-  public record RenameCourseRequest(
-    String name
+  public record ChangeCourseCapacityRequest(
+    Integer capacity
   ) {
   }
 
